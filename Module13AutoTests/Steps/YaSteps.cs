a691312b4d0fa39b9c941a77c039fbc6be8d107d
Aliaksandr_Lukashevi <aliaksandr_lukashevich@epam.com>
2022-10-14 15:46:24 +0300

    Add new 'Steps' layer

---
+++ Module13AutoTests/Steps/YaSteps.cs
@@ -0,0 +1,65 @@
+﻿using Module13AutoTests.Base;
+using Module13AutoTests.Pages;
+using NUnit.Framework;
+using System.Threading;
+
+namespace Module13AutoTests.Steps
+{
+	internal class YaSteps
+	{
+		static YaHome yaHomePage;
+		static YaLogin yaLoginPage;
+		static YaMailBox yaMailBoxPage;
+
+		public static void OpenHomePage()
+		{
+			Thread.Sleep(5000);
+			Driver.GetDriver().Navigate().GoToUrl("https://mail.yandex.ru/");
+			yaHomePage = new YaHome();
+			yaHomePage.WaitPageLoaded();
+			yaHomePage.ClickSignInButton();
+		}
+
+		public static void Login(string userName, string passwd)
+		{
+			OpenHomePage();
+			yaLoginPage = new YaLogin();
+			yaLoginPage.Login(userName, passwd);
+		}
+
+		public static void WaitForEmailReceived()
+		{
+			yaMailBoxPage = new YaMailBox();
+			yaMailBoxPage.WaitPageLoaded();
+			yaMailBoxPage.WaitForEmail();
+		}
+
+		public static void OpenUnreadEmail()
+		{
+			yaMailBoxPage = new YaMailBox();
+			yaMailBoxPage.OpenUnreadEmail();
+		}
+
+		public static void ReplyToEmail()
+		{
+			yaMailBoxPage = new YaMailBox();
+			yaMailBoxPage.ReplyEmail();
+			yaMailBoxPage.SendEmail();
+		}
+
+		public static void VerifyEmailContent(string subject, string sender)
+		{
+			yaMailBoxPage = new YaMailBox();
+			string subjectActual = yaMailBoxPage.GetSubject();
+			Assert.AreEqual(subject, subjectActual, "Subject is not correct");
+			string senderActual = yaMailBoxPage.GetSender();
+			Assert.AreEqual(sender, senderActual, "Sender is not correct");
+		}
+
+		public static void VerifyEmailIsSent()
+		{
+			yaMailBoxPage = new YaMailBox();
+			Assert.IsFalse(yaMailBoxPage.IsSendButtonDisplayed(), "Failed to send email");
+		}
+	}
+}
--- Module13AutoTests/Test/SendEmail.cs
+++ Module13AutoTests/Test/SendEmail.cs
@@ -1,15 +1,11 @@
 using Module13AutoTests.Base;
-using Module13AutoTests.Pages;
+using Module13AutoTests.Steps;
 using NUnit.Framework;
 
 namespace Module13AutoTests.Test
 {
 	public class SendEmailTests
 	{
-		const string _emailAddressOutlook = "automationtestuser1@outlook.com";
-		const string _emailAddressYa = "automationtestuser0@ya.ru";
-		const string _emailSubject = "Module13";
-
 		[SetUp]
 		public void Setup()
 		{
@@ -19,53 +15,27 @@ namespace Module13AutoTests.Test
 		[Test]
 		public void SendEmail()
 		{
-			//Login to Outlook
-			OutlookLogin outlookLoginPage = new OutlookLogin();
-			outlookLoginPage.Open();
-			outlookLoginPage.Login();
-			OutlookMailBox outlookMailBoxPage = new OutlookMailBox();
-			outlookMailBoxPage.WaitPageLoaded();
+			const string emailAddressOutlook = "automationtestuser1@outlook.com";
+			const string emailAddressYa = "automationtestuser0@ya.ru";
+			const string passwdOutlook = "Autotest123";
+			const string passwdYa = "Autotest123";
+			const string emailSubject = "Module13";
 
-			//Send new email
-			outlookMailBoxPage.CreateNewMessage();
-			outlookMailBoxPage.WaitPageLoaded();
+			OutlookSteps.Login(emailAddressOutlook, passwdOutlook);
+			OutlookSteps.CreateAndSendNewEmail(emailAddressYa, emailSubject);
 
-			//Login to Ya
-			YaHome yaHomePage = new YaHome();
-			yaHomePage.Open();
-			yaHomePage.ClickSignInButton();
-			YaLogin yaLoginPage = new YaLogin();
-			yaLoginPage.Login();
+			YaSteps.Login(emailAddressYa, passwdYa);
+			YaSteps.WaitForEmailReceived();
+			YaSteps.OpenUnreadEmail();
+			YaSteps.VerifyEmailContent(emailSubject, emailAddressOutlook);
+			
+			YaSteps.ReplyToEmail();
+			YaSteps.VerifyEmailIsSent();
 
-			//wait for email received
-			YaMailBox yaMailBoxPage = new YaMailBox();
-			yaMailBoxPage.WaitPageLoaded();
-			yaMailBoxPage.WaitForEmail();
-			yaMailBoxPage.OpenUnreadEmail();
-
-			//check email content
-			string subject = yaMailBoxPage.GetSubject();
-			Assert.AreEqual(_emailSubject, subject, "Subject is not correct");
-			string sender = yaMailBoxPage.GetSender();
-			Assert.AreEqual(_emailAddressOutlook, sender, "Sender is not correct");
-
-			//reply to email
-			yaMailBoxPage.ReplyEmail();
-			Assert.IsTrue(yaMailBoxPage.SendEmail(), "Failed to reply email");
-
-			//go to Outlook
-			Driver.GetDriver().Navigate().GoToUrl("https://www.outlook.com");
-
-			//wait for email received
-			outlookMailBoxPage.WaitPageLoaded();
-			outlookMailBoxPage.WaitForEmail();
-
-			//check email content
-			outlookMailBoxPage.OpenUnreadEmail();
-			subject = outlookMailBoxPage.GetSubject();
-			Assert.AreEqual("Re: " + _emailSubject, subject, "Subject is not correct");
-			sender = outlookMailBoxPage.GetSender();
-			Assert.IsTrue(sender.Contains(_emailAddressYa), "Sender is not correct");
+			OutlookSteps.OpenStartPage();
+			OutlookSteps.WaitForEmailReceived();
+			OutlookSteps.OpenUnreadEmail();
+			OutlookSteps.VerifyEmailContent("Re: " + emailSubject, emailAddressYa);
 		}
 
 		[TearDown]
